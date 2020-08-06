921e6a605154d5c5bdf53b0549fe533617de385a
Tierra <tierrarucker@ymail.com>
2020-08-06 01:08:34 -0400

    update files

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Project Goal: 
+
+When a URL link is typed or pasted into the input box and the  
+*/
+
+// 
+
+// function when button is click
+function myFunction() {
+  var imageLink = document.getElementById("imageLink").value;
+  //alert(imageLink);
+}
+
