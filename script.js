65bf0f3f213f720d148f32e286c9d55bb9cd067a
Tierra <tierrarucker@ymail.com>
2020-08-09 12:32:07 -0400

    updated script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ When a URL link is typed or pasted into the input box and the
 // function when button is click
 function myFunction() {
   var imageLink = document.getElementById("imageLink").value;
-  //alert(imageLink);
+  alert(imageLink);
+
 }
 
+// display-image-div
