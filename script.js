1bf461f78732996fff13af9e38a9429bc40b6945
Tierra <tierrarucker@ymail.com>
2020-08-12 22:29:39 -0400

    updated script.js

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ When a URL link is typed or pasted into the input box and the
 // 
 
 // function when button is click
-function myFunction() {
-  var imageLink = document.getElementById("imageLink").value;
-  alert(imageLink);
+// function myFunction() {
+//   var imageLink = document.getElementById("imageLink").value;
+//   alert(imageLink);
 
-}
+// }
 
 // display-image-div
+
+/* What's your age? */
+
+var age = prompt("What's your age?");
+alert(age);
