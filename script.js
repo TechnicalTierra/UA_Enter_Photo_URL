26493e7ef091e2bcd683cc344818207d33c66ad7
Tierra <50035689+RuckerTierra@users.noreply.github.com>
2020-11-04 19:38:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-/* Project Goal: When a URL link is typed or copy and pasted into the input box and the user clicks the "Click to Display Image Below" button or presses "Enter" then the image will display in the box (div) below.
+/* 
+Project: When a URL link is typed or copy and pasted into the input box and the user clicks the "Click to Display Image Below" button or presses "Enter" then the image will display in the box below.
 */
 
-// 
+// Step 1: Get and store the URL input in a variable: DONE
+// *Only works inside the function
 
-// function when button is click
+// Step 2: Create a function to get the urlInput value: DONE
+// Step 3: On mouse click of "Display button" run function: DONE
 function showImage() {
-  var urlInput = document.getElementById("urlInput").value;
-
-
+  let urlInput = document.getElementById("urlInput").value;
+  alert(urlInput)
+  document.getElementById("urlInput").value = "";
 }
 
-var dynamicImage; 
+// Step 4: On "Enter" press run function: IN PROGRESS
+function showImage() {
+  let urlInput = document.getElementById("urlInput").value;
+  alert(urlInput)
+  document.getElementById("urlInput").value = "";
+}
+
+
+// Project: IN PROGRESS
+
+
